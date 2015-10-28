eb9dd536f1e710ca92253943b7527643321575df
Ethan Yonker <dees_troy@teamw.in>
2015-10-27 22:09:50 -0500

    Initial c commit

---
+++ omni_angler.mk
@@ -0,0 +1,45 @@
+#
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
+PRODUCT_COPY_FILES += \
+    device/huawei/angler/kernel:kernel
+
+PRODUCT_NAME := omni_angler
+PRODUCT_DEVICE := angler
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+
+# Kernel inline build
+#TARGET_KERNEL_CONFIG := angler_defconfig
+#TARGET_VARIANT_CONFIG := angler_defconfig
+#TARGET_SELINUX_CONFIG := angler_defconfig
